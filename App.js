157b784c40e9561f707651344e3a95f7429e1282
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-09-28 18:51:02 +0700

    1a completed

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cau1a from './components/1_a';
+
+export default function App() {
+  return (
+    <Cau1a />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+//   box1: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     justifyContent: 'space-between' 
+//   },
+//   item: {
+//     justifyContent: 'space-around',
+//     width: '10%'
+//   }
+// });
