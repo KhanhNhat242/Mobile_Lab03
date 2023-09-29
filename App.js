b1f490e6cbca6e8deea27139f3b2bb77e708e1ba
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-09-29 20:42:06 +0700

    1e completed

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import Cau1a from './components/1_a';
 import Cau1b from './components/1_b';
 import Cau1c from './components/1_c';
 import Cau1d from './components/1_d';
+import Cau1e from './components/1_e';
 
 export default function App() {
   return (
     // <Cau1a />
     // <Cau1b />
     // <Cau1c />
-    <Cau1d />
+    // <Cau1d />
+    <Cau1e />
   );
 }
 
