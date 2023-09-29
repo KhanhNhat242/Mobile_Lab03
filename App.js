5572879f23376f78e468128187bd02e2deb27c44
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-09-29 19:44:19 +0700

    1b, 1c, 1d completed

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Cau1a from './components/1_a';
+import Cau1b from './components/1_b';
+import Cau1c from './components/1_c';
+import Cau1d from './components/1_d';
 
 export default function App() {
   return (
-    <Cau1a />
+    // <Cau1a />
+    // <Cau1b />
+    // <Cau1c />
+    <Cau1d />
   );
 }
 
